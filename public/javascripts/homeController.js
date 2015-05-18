8f03a18f007f2d33712c121d7d27f82fc4282535
asadighi <sadighi.aidin@gmail.com>
2015-05-17 18:32:30 -0700

    ensure player names are validated

--- public/javascripts/homeController.js
+++ public/javascripts/homeController.js
@@ -2,6 +2,11 @@ app.controller('HomeController', ['$scope', 'Games', '$location', function ($sco
 
 
 	$scope.newGame = function() {
+
+		if (!$scope.playerName1 ||  !$scope.playerName2)	{
+			alert('Player names are required');
+			return;
+		}
 		var game = new Games({ playerName1: $scope.playerName1, playerName2: $scope.playerName2});
         		game.$save(function(newGame){
                  	var gameId = newGame._id;
